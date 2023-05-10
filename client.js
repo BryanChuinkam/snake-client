091545f495be802b5833cf20883f0fa39db73877
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-10 18:45:01 +0000

    added writing initials to server side functionality

--- client.js
+++ client.js
@@ -10,9 +10,15 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log("Connection successfully established!");
+  });
+
   conn.on('data', (data) => {
     console.log('Incoming data: ', data);
   });
+
+  conn.write("Name: BNC"); 
 
   return conn;
 };
